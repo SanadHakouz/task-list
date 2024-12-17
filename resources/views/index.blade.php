967a82307ccf28af143ede09b377e36c48ef55ca
Sanad <jzokah@yahoo.com>
2024-12-17 15:49:12 +0300

    Initial commit for task-list project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('title','the list of tasks')
+
+
+@section('content')
+
+ <nav class="mb-4">
+ <a href="{{route('tasks.create')}}"
+ class="link">
+ Add Task</a>
+ </nav>
+    {{-- @if (count($tasks)) --}}
+    @forelse ($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+        @class(['font-bold','line-through' => $task->completed])>{{ $task->title }}</a>
+
+    </div>
+    @empty
+      <div>There are no tasks!</div>
+    @endforelse
+    {{-- @endif --}}
+
+    @if ($tasks->count())
+    <nav class="mt-4">
+        {{ $tasks->links() }}
+    </nav>
+    @endif
+@endsection
+
